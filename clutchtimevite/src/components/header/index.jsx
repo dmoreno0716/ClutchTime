e75d6b73a9aa0db93681a2bbc4a4de429b6b1d62
David Moreno <dmore107@fiu.edu>
2024-07-03 15:41:31 -0700

    transfered react code to vite

--- clutchtimevite/src/components/header/index.jsx
+++ clutchtimevite/src/components/header/index.jsx
@@ -1,55 +1,61 @@
-import React from 'react'
-import { Link, useNavigate } from 'react-router-dom'
-import { useAuth } from '../../contexts/authContext'
-import { doSignOut } from '../../firebase/auth'
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useAuth } from "../../contexts/authContext";
+import { doSignOut } from "../../firebase/auth";
 
 const Header = () => {
-    const navigate = useNavigate()
-    const { userLoggedIn } = useAuth()
+  const navigate = useNavigate();
+  const { userLoggedIn } = useAuth();
 
-    const navStyle = {
-        display: 'flex',
-        flexDirection: 'row',
-        gap: '8px',
-        width: '100%',
-        zIndex: '20',
-        top: '0',
-        left: '0',
-        height: '48px',
-        borderBottom: '1px solid #E5E7EB',
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#E5E7EB'
-    }
+  const navStyle = {
+    display: "flex",
+    flexDirection: "row",
+    gap: "8px",
+    width: "100%",
+    zIndex: "20",
+    top: "0",
+    left: "0",
+    height: "48px",
+    borderBottom: "1px solid #E5E7EB",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#E5E7EB",
+  };
 
-    const linkStyle = {
-        fontSize: '14px',
-        color: '#2563EB',
-        textDecoration: 'underline',
-        cursor: 'pointer'
-    }
+  const linkStyle = {
+    fontSize: "14px",
+    color: "#2563EB",
+    textDecoration: "underline",
+    cursor: "pointer",
+  };
 
-    return (
-        <nav style={navStyle}>
-            {
-                userLoggedIn
-                    ?
-                    <>
-                        <button 
-                            onClick={() => { doSignOut().then(() => { navigate('/login') }) }} 
-                            style={linkStyle}
-                        >
-                            Logout
-                        </button>
-                    </>
-                    :
-                    <>
-                        <Link style={linkStyle} to={'/login'}>Login</Link>
-                        <Link style={linkStyle} to={'/register'}>Register New Account</Link>
-                    </>
-            }
-        </nav>
-    )
-}
+  return (
+    <nav style={navStyle}>
+      {userLoggedIn ? (
+        <>
+          <button
+            onClick={() => {
+              doSignOut().then(() => {
+                navigate("/login");
+              });
+            }}
+            style={linkStyle}
+          >
+            Logout
+          </button>
+        </>
+      ) : (
+        <>
+          <Link style={linkStyle} to={"/login"}>
+            Login
+          </Link>
+          <Link style={linkStyle} to={"/register"}>
+            Register New Account
+          </Link>
+        </>
+      )}
+    </nav>
+  );
+};
 
 export default Header;
