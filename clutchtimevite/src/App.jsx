c4dd49dbf010bd1de9248cca5f4b88119785b7c5
David Moreno <dmore107@fiu.edu>
2024-07-03 15:41:31 -0700

    transfered react code to vite

---
+++ clutchtimevite/src/App.jsx
@@ -0,0 +1,28 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from "./components/auth/login";
+import Register from "./components/auth/register";
+import Header from "./components/header";
+import Home from "./components/home";
+import './App.css';
+import { AuthProvider } from "./contexts/authContext";
+
+function App() {
+  return (
+    <Router>
+      <AuthProvider>
+        <Header />
+        <div className="App-content">
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="*" element={<Login />} />
+          </Routes>
+        </div>
+      </AuthProvider>
+    </Router>
+  );
+}
+
+export default App;
