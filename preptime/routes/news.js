3383fdaedbc731335f062bc02ece4206687af08d
David Moreno <dmore107@fiu.edu>
2024-07-15 16:01:33 -0700

    creating backend routes for each different league news

--- preptime/routes/news.js
+++ preptime/routes/news.js
@@ -14,12 +14,52 @@ router.get("/source/:source", async (req, res) => {
   res.send(response.data);
 });
 
-router.get("source/:source", async (req, res) => {
-  const { source } = req.params;
-  const response = await axios.get(
-    `https://footballnewsapi.netlify.app/.netlify/functions/api/news/fourfourtwo/${source}`
-  );
-  res.send(response.data);
+router.get("/fourfourtwo/laliga", async (req, res) => {
+  try {
+    const response = await axios.get(
+      `https://footballnewsapi.netlify.app/.netlify/functions/api/news/fourfourtwo/laliga`
+    );
+    res.send(response.data);
+  } catch (error) {
+    console.error("Error fetching LaLiga news: ", error);
+    res.status(500).send("Error fetching LaLiga news");
+  }
+});
+
+router.get("/fourfourtwo/epl", async (req, res) => {
+  try {
+    const response = await axios.get(
+      `https://footballnewsapi.netlify.app/.netlify/functions/api/news/fourfourtwo/epl`
+    );
+    res.send(response.data);
+  } catch (error) {
+    console.error("Error fetching EPL news: ", error);
+    res.status(500).send("Error fetching EPL news");
+  }
+});
+
+router.get("/fourfourtwo/bundesliga", async (req, res) => {
+  try {
+    const response = await axios.get(
+      `https://footballnewsapi.netlify.app/.netlify/functions/api/news/fourfourtwo/bundesliga`
+    );
+    res.send(response.data);
+  } catch (error) {
+    console.error("Error fetching Bundesliga news: ", error);
+    res.status(500).send("Error fetching Bundesliga news");
+  }
+});
+
+router.get("/fourfourtwo/ucl", async (req, res) => {
+  try {
+    const response = await axios.get(
+      `https://footballnewsapi.netlify.app/.netlify/functions/api/news/fourfourtwo/ucl`
+    );
+    res.send(response.data);
+  } catch (error) {
+    console.error("Error fetching UCL news: ", error);
+    res.status(500).send("Error fetching UCL news");
+  }
 });
 
 module.exports = router;
